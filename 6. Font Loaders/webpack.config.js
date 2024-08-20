652bb42b8912f889b43fecd3145814c0a9be6466
Sudhir Singh <56826370+Sudhirsin@users.noreply.github.com>
2024-08-20 16:57:54 +0530

    font loader

---
+++ 6. Font Loaders/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: 'images/[hash][ext]', // all the images will store inside the images folder
+    clean: true // It will remove the file that is not being used (images)
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css)$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader", options: { modules: true } },
+        ],
+      },
+      {
+        test: /.s[ac]ss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader", options: { modules: true } },
+          { loader: "sass-loader" },
+        ],
+      },
+      {
+        test: /.(png|jpeg|gif|svg)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /.(ttf|woff|woff2)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
