40a62757e6f2e50906fab381f5fb613a2ace9bd1
Sudhir Singh <56826370+Sudhirsin@users.noreply.github.com>
2024-08-23 11:36:11 +0530

    images load issue

--- 8.3 Course Project_webpack-dev-server/src/index.js
+++ 8.3 Course Project_webpack-dev-server/src/index.js
@@ -4,6 +4,7 @@ import bootstrap from 'bootstrap';
 import Typed from 'typed.js';
 import validate from 'validate.js';
 import toastr from 'toastr';
+// import banner from './assets/banner-image.png'
 
 import './index.css'
 import './_vendor.scss'
@@ -56,3 +57,7 @@ const footerCol3 = $("#footer-column-3")[0];
 buildFooterItems(footerCol1, footerCol1Items);
 buildFooterItems(footerCol2, footerCol2Items);
 buildFooterItems(footerCol3, footerCol3Items);
+
+// 1 st way to load the images
+// const bannerImg = $('#bannerImg')[0];
+// bannerImg.src = banner
--- 8.3 Course Project_webpack-dev-server/webpack.config.js
+++ 8.3 Course Project_webpack-dev-server/webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
         test: /\.s[ac]ss$/,
         use: ['style-loader', 'css-loader', 'sass-loader']
       },
+      {
+        test: /\.(png|jpeg|jpg|gif|svg)$/,
+        use: "asset/resource"
+      },
     ]
   },
   plugins: [
