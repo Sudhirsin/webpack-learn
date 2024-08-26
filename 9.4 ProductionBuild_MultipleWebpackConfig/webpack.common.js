ac2908a909500ac2ff5fcc1ccc15b5fa956a9b06
Sudhir Singh <56826370+Sudhirsin@users.noreply.github.com>
2024-08-26 22:41:14 +0530

    splitting the webpack files in  multiple to support dev and prod

---
+++ 9.4 ProductionBuild_MultipleWebpackConfig/webpack.common.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
+
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+    courses: "./src/pages/courses.js",
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpeg|jpg|gif)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      mnt: 'moment',
+      $: 'jquery'
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      chunks: ["index"],
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/courses.html",
+      chunks: ["courses"],
+      filename: "courses.html",
+      base: "pages",
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+};
